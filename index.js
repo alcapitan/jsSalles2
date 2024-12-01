ccf4ffb8cea340050687c805b2412d791a97803a
Tidic <tidic84@gmail.com>
2024-12-01 16:24:16 +0100

    ajout du choix de l'heure

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/salles/public',express.static(path.join(__dirname, 'public')));
 
 app.get('/salles', async (req, res) => {
+    if(req.query.date || req.query.time) {
+        const heureRegex = /^[0-2][0-9]:[0-5][0-9]$/;
+        const dateRegex = /\d{4}-\d{2}-\d{2}/;
+        if(!heureRegex.test(req.query.time) || !dateRegex.test(req.query.date)) {
+            res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+            return;
+        }
+    }
     try {
-        const { freeRooms, usedRooms } = await getFreeRooms();
-        // for(const room in usedRooms) {
-        //     console.log(toDate(usedRooms[room].courses.willBeFree));
-        // }
+        
+        const { freeRooms, usedRooms } = await getFreeRooms(req.query.date, req.query.time);
+
         const sortedFreeRooms = Object.keys(freeRooms).sort().reduce((result, key) => {
             result[key] = freeRooms[key];
             return result;
@@ -31,6 +38,7 @@ app.get('/salles', async (req, res) => {
         res.status(500).json({ error: 'Erreur lors de la récupération des salles libres' });
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Serveur en écoute sur le port ${port}`);
