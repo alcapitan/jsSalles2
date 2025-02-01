c2496b1c02effa578397770d31fc857ea6e6decd
Tidic <tidic84@gmail.com>
2025-02-01 16:57:28 +0100

    .env

--- js/sql.js
+++ js/sql.js
@@ -1,13 +1,15 @@
 const { Client } = require('pg');
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const createClient = () => {
     return new Client({
-        user: 'jssalles',
-        host: 'db.tidic.fr',
-        database: 'jssalles',
-        password: 'xa32vc8b',
-        port: 5432,
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
     });
 };
 
